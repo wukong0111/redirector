e153b9969c3cc5d0688f7453e32f016e23ef77fb
Daniel Muñoz Morales <dmorales@inespay.com>
2023-09-13 20:12:57 +0200

    add logger middleware

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"net/url"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	e.GET("/", func(c echo.Context) error {
 		code := c.QueryParam("code")
 		state := c.QueryParam("state")
